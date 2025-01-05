383ce821df286f2faa0df5654e5039d90befdb02
Atte <162895880+Atte-A@users.noreply.github.com>
2025-01-05 21:18:40 +0200

    uibuild

--- index.js
+++ index.js
@@ -3,60 +3,84 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 
-const morgan = require('morgan');
-const cors = require('cors');
 const Person = require('./models/person');
+
+app.use(express.static('dist'));
+
+const morgan = require('morgan');
 
 morgan.token('data', function (request, response) {
   return JSON.stringify(request.body);
 });
 
+const errorHandler = (error, request, response, next) => {
+  console.log(error.message);
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' });
+  }
+
+  next(error);
+};
+
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json());
 app.use(
   morgan(':method :url :status :res[content-length] - :response-time ms :data'),
 );
 
-app.use(cors());
-app.use(express.json());
-app.use(express.static('dist'));
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' });
+};
 
-app.get('/info', (request, response) => {
+app.get('/info', (request, response, next) => {
   const date = new Date();
 
-  Person.countDocuments({}).then((personCount) => {
-    response.send(`
+  Person.countDocuments({})
+    .then((personCount) => {
+      response.send(`
       <div>
         <p>Phonebook has info for ${personCount} people</p>
         <p>${date}</p>
       </div>`);
-  });
+    })
+    .catch((error) => next(error));
 });
 
-app.get('/api/persons', (request, response) => {
-  Person.find({}).then((persons) => {
-    response.json(persons);
-  });
+app.get('/api/persons', (request, response, next) => {
+  Person.find({})
+    .then((persons) => {
+      response.json(persons);
+    })
+    .catch((error) => next(error));
 });
 
-app.get('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id).then((person) => {
-    if (person) {
-      response.json(person);
-    } else {
-      response.status(404).end();
-    }
-  });
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (person) {
+        response.json(person);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 
-app.delete('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id).then((person) => {
-    if (person) {
-      person.deleteOne().then(() => {
-        response.status(204).end();
-      });
-    } else {
-      response.status(404).end();
-    }
-  });
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (person) {
+        person.deleteOne().then(() => {
+          response.status(204).end();
+        });
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 
 app.post('/api/persons', (request, response) => {
@@ -76,15 +100,40 @@ app.post('/api/persons', (request, response) => {
       number,
     });
 
-    newPerson.save().then((savedPerson) => {
-      response.json(savedPerson);
-    });
+    newPerson
+      .save()
+      .then((savedPerson) => {
+        response.json(savedPerson);
+      })
+      .catch((error) => next(error));
   });
 });
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const { name, number } = request.body;
+
+  const updatedPerson = {
+    name,
+    number,
+  };
+
+  Person.findByIdAndUpdate(request.params.id, updatedPerson, {
+    new: true,
+  })
+    .then((updatedPerson) => {
+      if (updatedPerson) {
+        response.json(updatedPerson);
+      }
+    })
+    .catch((error) => next(error));
+});
+
+app.use(unknownEndpoint);
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
-// Jatka "Virheiden käsittely"
+// Validointi ja ESLint
